058d9b3ab64221018033f20f472927c3b9797ea8
Palani Nagarajan <npalania@amazon.com>
2023-01-27 11:04:45 -0800

    [Execute notebook magic] Add link in the cell output to open output notebook, add more details in the help

--- emr_notebooks_magics/execute_emr_notebook.py
+++ emr_notebooks_magics/execute_emr_notebook.py
@@ -6,10 +6,14 @@ from IPython.core import magic_arguments
 from IPython.core.magic import (Magics, magics_class, line_magic)
 from IPython.core.error import UsageError
 from .utils.instance_metadata_service_utils import IMDSv2Util
+from IPython.display import display, HTML
 
+EXECUTIONS_TERMINAL_STATUS = ["FINISHED", "FAILED"]
+EXECUTIONS_STARTING_STATUS = ["STARTING", "START_PENDING"]
 
 @magics_class
 class ExecuteNotebookMagics(Magics):
+
     def __init__(self, shell):
         super(ExecuteNotebookMagics, self).__init__(shell)
         self.shell = shell
@@ -43,6 +47,12 @@ class ExecuteNotebookMagics(Magics):
     def execute_notebook(self, line):
         """
         Execute a EMR Studio Notebook non-interactively.
+        Followings permissions are required on EMR-EC2 role to execute this magic
+        `
+           "elasticmapreduce:StartNotebookExecution",
+           "elasticmapreduce:DescribeNotebookExecution",
+           "iam:PassRole"
+        `
         """
 
         args = magic_arguments.parse_argstring(self.execute_notebook, line)
@@ -72,13 +82,31 @@ class ExecuteNotebookMagics(Magics):
         # Polling for execution to finish
         start = time.time()
 
+        is_nb_output_link_shown = False
+
         # Note: Timeout will NOT be precise as there is 15s sleep inside the loop.
         while (time.time() - start) < timeout:
             describe_response = self.emr.describe_notebook_execution(NotebookExecutionId=notebook_execution_id)
-            if describe_response["NotebookExecution"]["Status"] == "FINISHED" or describe_response["NotebookExecution"]["Status"] == "FAILED":
-                print("Execution completed. Status: ", describe_response["NotebookExecution"]["Status"])
+            execution_status = describe_response["NotebookExecution"]["Status"]
+            if execution_status in EXECUTIONS_TERMINAL_STATUS:
+                print("Execution completed. Status: ", execution_status)
                 break
+
+            if not is_nb_output_link_shown and execution_status not in EXECUTIONS_STARTING_STATUS:
+                workspace_relative_path = self.get_output_nb_workspace(describe_response["NotebookExecution"]["OutputNotebookURI"])
+                if workspace_relative_path is not None:
+                    html = HTML("""<a href="{}">Click here</a> to view the output Notebook file. (The output cell will be empty until the cell is executed)"""
+                                .format(workspace_relative_path)
+                                )
+                    display(html)
+                is_nb_output_link_shown = True
             time.sleep(15)
+
+    def get_output_nb_workspace(self, output_notebook_uri):
+        workspace_s3_prefix = os.environ["KERNEL_WORKSPACE_DIR_S3_PREFIX"]
+        if output_notebook_uri.startswith(workspace_s3_prefix):
+            return output_notebook_uri[len(workspace_s3_prefix):]
+        return None
 
     def get_cluster_id(self):
         ec2_instance_id = self.imdsv2.ec2_instance_id()
